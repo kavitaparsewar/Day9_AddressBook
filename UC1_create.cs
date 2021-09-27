a9c3f3136e84bdc304ff120e53b7c88b858c706f
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-27 20:46:41 +0530

    [kavita]Added create a contacts in address book program

---
+++ UC1_create.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_AddressBook
+{
+    class UC1_create
+    {
+        public string fname, lname, addr, city, state, email;
+        public static void Book()
+        {
+            UC1_create b = new UC1_create();
+
+            Console.WriteLine("Enter first name : ");
+            b.fname = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            b.lname = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            b.addr = Console.ReadLine();
+
+            Console.WriteLine("Enter City: ");
+            b.city = Console.ReadLine();
+
+            Console.WriteLine("Enter state: ");
+            b.state = Console.ReadLine();
+
+            Console.WriteLine("Enter email: ");
+            b.email = Console.ReadLine();
+
+            Console.WriteLine("Enter zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone number");
+            int phone = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
