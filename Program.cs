9277aaa460c2817ffbbe6cb91c3e24b6aff66e66
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-28 20:27:37 +0530

    [kavita] program.cs file added

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day9_AddressBook
+{
+    class Program
+    {
+        public static void Main(String[] args)
+
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            //UC1_create.Book();
+
+            //#region UC2
+            //UC2_addnew.Book1();
+            //UC2_addnew.Print();
+            //#endregion UC2
+
+            //#region UC3
+            //UC3_edit.editdetails();
+            //UC3_edit.All();
+            //UC3_edit.Update();
+            //#endregion UC3
+
+
+            //region UC4
+            //UC4_delete.createnew();
+            //UC4_delete.Contacts();
+            //UC4_delete.deletePerson();
+            //endregion UC4
+
+        }
+    }
+}
